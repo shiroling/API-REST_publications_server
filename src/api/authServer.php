2892105abe487fd542d9428d08cab67a9fe9c4a1
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-03-07 16:17:14 +0100

    debut serveurs

---
+++ src/api/authServer.php
@@ -0,0 +1,13 @@
+<?php
+
+    header("Content-Type:application/json");
+
+    $http_method = $_SERVER['REQUEST_METHOD'];
+    switch($http_method){
+        case 'POST':
+            break;
+        default:
+            break;
+    }
+
+?>
