98bb41e76833337fb8857d44024933c394a59ddb
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-03-09 08:10:28 +0100

    auth + début de app

--- src/api/appServer.php
+++ src/api/appServer.php
@@ -1,19 +1,54 @@
 <?php
 
+    require('../dependencies/jwt-utils.php');
+
     header("Content-Type:application/json");
 
+    $bearer_token = '';
+    $bearer_token = get_bearer_token();
     $http_method = $_SERVER['REQUEST_METHOD'];
+
     switch($http_method){
-        case 'GET':
+        case 'GET': //EVERYONE
+            if (is_jwt_valid($bearer_token)) {
+
+            } else {
+                
+            }
             break;
-        case 'POST':
+        case 'POST': //PUBLISHER
+            if (is_jwt_valid($bearer_token)) {
+                
+            } else {
+                deliver_response(503, "Utilisateur non connecte", NULL);
+            }
             break;
-        case 'PUT':
+        case 'PUT': //PUBLISHER
+            if (is_jwt_valid($bearer_token)) {
+
+            } else {
+                deliver_response(503, "Utilisateur non connecte", NULL);
+            }
             break;
-        case 'DELETE':
+        case 'DELETE': //MODERATOR - PUBLISHER
+            if (is_jwt_valid($bearer_token)) {
+
+            } else {
+                deliver_response(503, "Utilisateur non connecte", NULL);
+            }
             break;
         default:
+            deliver_response(405, "Methode non supportee", NULL);
             break;
+    }
+
+    function deliver_response($status, $status_message, $data){
+        header("HTTP/1.1 $status $status_message");
+        $response['status'] = $status;
+        $response['status_message'] = $status_message;
+        $response['data'] = $data;
+        $json_response = json_encode($response);
+        echo $json_response;
     }
 
 ?>
--- src/api/authServer.php
+++ src/api/authServer.php
@@ -1,10 +1,22 @@
 <?php
+    require('../dependencies/jwt-utils.php');
+    require('../dataModel/Utilisateur.php');
 
     header("Content-Type:application/json");
 
     $http_method = $_SERVER['REQUEST_METHOD'];
-    switch($http_method){
+    switch($http_method) {
         case 'POST':
+            $data = (array) json_decode(file_get_contents('php://input'), true);
+            if (isValidUser($data['username'], $data['password'])) {
+                $user = getUserInfo();
+                $headers = array('alg'=>'HS256', 'typ'=>'JWT');
+                $payload = array('id'=>$user[0]['id'], 'role'=>$user[0]['role'], 'exp'=>(time() + 31536000));
+                $jwt = generate_jwt($headers, $payload);
+                deliver_response(200, "JWT cree", $jwt);
+            } else {
+                deliver_response(404, "Utilisateur introuvable", null);
+            }
             break;
         default:
             break;
