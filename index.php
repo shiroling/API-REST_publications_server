bb10316d7c60f6df44cee357708d0c93978c1475
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-03-16 09:29:52 +0100

    début du client

--- index.php
+++ index.php
@@ -7,6 +7,32 @@
 </head>
 <body>
 
+	<?php
+
+		$postsAnonymous = json_decode(file_get_contents('http://localhost/R4.01/Projet/src/api/appServer.php',
+			false,
+			stream_context_create(array('http' => array('method' => 'GET')))
+			), true);
+
+		echo '<table>
+				<thead>
+					<tr>
+						<th>Auteur</th>
+						<th>Contenu</th>
+						<th>Date</th>
+					</tr>
+				</thead>
+				<tbody>';
+		foreach($postsAnonymous['data'] as $item) {
+			echo '<tr>
+					<td>'.htmlentities($item['nom']).'</td>
+					<td>'.htmlentities($item['contenu']).'</td>
+					<td>'.htmlentities($item['date']).'</td>
+				</tr>';
+		}
+		echo '</tbody>
+			</table>';
+	?>
 
 </body>
 </html>
